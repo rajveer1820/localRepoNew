2722d6d911f7993e61c8cb534052886a832df8e2
Rajveer-rvs <rs5656singh@gmail.com>
2026-05-29 14:39:55 +0530

    adding thread File

---
+++ Thread.java
@@ -0,0 +1,9 @@
+package localRepoNew;
+
+public class Thread {
+    public static void main(String[] args) {
+        for(int i=0; i<100; i++){
+            System.out.println("hello");
+        }
+    }
+}
